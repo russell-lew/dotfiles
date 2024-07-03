d26ce950a75d1570bdc39dd40c6cc576b2780750
Russell Lew <russell.lyf@gmail.com>
2024-07-03 23:36:36 +0800

    fix setup script with folder variable

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Clone the dotfiles repository
-folder = '.dotfiles'
-if ! git clone https://github.com/russell-lew/dotfiles.git ~/${folder} 2>/dev/null && [ -d "${folder}"] ; then 
-	echo '[SETUP] Clone failed because the folder ${folder} already exists'
+folder="~/.dotfiles"
+if ! git "clone https://github.com/russell-lew/dotfiles.git" "${folder}" 2>/dev/null && [ -d "${folder}"] ; then 
+	echo "[SETUP] Clone failed because the folder ${folder} already exists"
 fi
 
 # Create symlinks for tmux and nvim configuration
 echo '[SETUP] Creating Symbolic links for tmux and nvim configuration'
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/nvim
-ln -sf ~/dotfiles/nvim/* ~/.config/nvim/
+ln -sf ~/.dotfiles/nvim/* ~/.config/nvim/
 
 # Install tmux and nvim if not already installed (example for Ubuntu)
 echo '[SETUP] Installing tmux and nvim'
