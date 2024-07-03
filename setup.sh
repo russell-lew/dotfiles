2372a350d4c54ff14151720a23ed0f545b6269df
Russell Lew <russell.lyf@gmail.com>
2024-07-04 00:28:21 +0800

    refactor setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,15 @@
 
 # Clone the dotfiles repository
 folder="~/.dotfiles"
-if ! git "clone https://github.com/russell-lew/dotfiles.git" "${folder}" 2>/dev/null && [ -d "${folder}"] ; then 
+if ! git "clone https://github.com/russell-lew/dotfiles.git" "${folder}" 2>/dev/null && [ -d "${folder}" ] ; then 
 	echo "[SETUP] Clone failed because the folder ${folder} already exists"
 fi
 
 # Create symlinks for tmux and nvim configuration
 echo '[SETUP] Creating Symbolic links for tmux and nvim configuration'
-ln -sf ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf "${folder}/tmux/.tmux.conf" ~/.tmux.conf
 mkdir -p ~/.config/nvim
-ln -sf ~/.dotfiles/nvim/* ~/.config/nvim/
+ln -sf "${folder}/nvim/*" ~/.config/nvim/
 
 # Install tmux and nvim if not already installed (example for Ubuntu)
 echo '[SETUP] Installing tmux and nvim'
