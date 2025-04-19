36cfeb207df1e5dac4ba1b1f00eb3ff1608bccb7
Russell Lew <russell.lyf@gmail.com>
2025-04-19 10:11:50 +0800

    Add leetcode plugin

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -20,4 +20,15 @@ return {
       { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
     },
   },
+  {
+    'kawre/leetcode.nvim',
+    build = ':TSUpdate html',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      -- "ibhagwan/fzf-lua",
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+    },
+    opts = {},
+  },
 }
